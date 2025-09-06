5896a713ed0f499eb1c9374c10b50016fe3ad334
Ananya Chennadi <ananyachennadi2@gmail.com>
2025-09-06 16:07:59 +0100

    feat: added get requests to app to get and store top artist based on time range given in parameter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
-import { useSearchParams } from "react-router-dom"
 import ArtistAnimation from "./components/ArtistAnimation";
 
 function App() {
-  // check if the user is authentication and use setLoginSuccess to indicate this
   const [loginSuccess, setLoginSuccess] = useState(false);
-  const [searchParams] = useSearchParams();
+  const [artistData, setArtistData] = useState({
+    short_term: [],
+    medium_term: [],
+    long_term: []
+  });
+
 
   useEffect(() => {
+    // check if user is authenticated by sending a request to /is-authenticated path and set login success
     const checkAuthStatus = async () => {
         try {
           const response = await fetch('https://127.0.0.1:5000/is-authenticated', {
@@ -27,19 +31,49 @@ function App() {
           setLoginSuccess(false);
         }
       };
-
+    
+      // fetch top artist data needed for artist animation and transform data into dictionary absed on given time-range
+    const fetchArtistData = async(time_range) => {
+        try {
+            const response = await fetch(`https://127.0.0.1:5000/artist-animated?time_range=${time_range}`, {
+                credentials: 'include'
+            });
+            
+            if (response.ok) {
+                const data = await response.json();
+                const transformedData = data.map((name, index) => ({
+                    name: name,
+                    value: 20 - (index * 2)
+                }));
+                setArtistData(prevData => ({
+                    ...prevData,
+                    [time_range]: transformedData
+                }));
+            } else {
+                console.error("Failed to fetch artist data. Status:", response.status);
+            }
+        } catch (error) {
+            console.error("Network or parsing error:", error);
+        }
+    };
+    
+    // Call the functions
     checkAuthStatus();
+    fetchArtistData('short_term');
+    fetchArtistData('medium_term');
+    fetchArtistData('long_term');
+
   }, []);
 
-  // if user is not authenticated render login component otherwise render dashboard
+  // if user is not authenticated render login component otherwise render dashboard and components in dashboard
   return (
-    <div className="flex w-[90%] h-[90%] mx-auto bg-[#f3f2ec] rounded-lg align-self m-8 md:m-10 lg:p-12">
+    <div className="flex h-full w-full">
       {!loginSuccess ? (
         <Login />
       ) : (
         <>
         <Dashboard />
-        <ArtistAnimation></ArtistAnimation>
+        <ArtistAnimation chartData={artistData}></ArtistAnimation>
         </>
       )}
     </div>
