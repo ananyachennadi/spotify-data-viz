41622839859a2311eb2276b1e256af909477d31d
Ananya Chennadi <ananyachennadi2@gmail.com>
2025-09-09 21:23:42 +0100

    feat: removed certs from vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, '..', 'flask-server', 'certs', 'localhost+1-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, '..', 'flask-server', 'certs', 'localhost+1.pem')),
-    },
-    host: '127.0.0.1',
-    port: 5173,
-  }
-})
+});
